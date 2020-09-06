4cc9ab8bdd75e8b37e39b4d20845e229eae7cc19
Weston Eckloff <42098977+Yellowstoner@users.noreply.github.com>
2020-09-06 16:47:40 -0600

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,6 +1,5 @@
 // D-3 Challenge
 
-// Automatically resize the chart
 // if the SVG area isn't empty when the browser loads,
 // remove it and replace it with a resized version of the chart
 var svgArea = d3.select("body").select("svg");
